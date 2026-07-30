1ca09295c0c81f48c88fc48c441f37b343628c5d
mdt143785-afk <mdt143785@gmail.com>
2026-07-30 10:06:15 +0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,71 @@
+import { auth } from "./firebase.js";
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+}
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+
+window.register = function(){
+
+let email =
+document.getElementById("email").value;
+
+let password =
+document.getElementById("password").value;
+
+
+createUserWithEmailAndPassword(
+auth,
+email,
+password
+)
+
+.then(()=>{
+
+document.getElementById("msg").innerHTML =
+"✅ Account তৈরি হয়েছে";
+
+})
+
+.catch((error)=>{
+
+document.getElementById("msg").innerHTML =
+error.message;
+
+});
+
+}
+
+
+
+window.login = function(){
+
+let email =
+document.getElementById("email").value;
+
+let password =
+document.getElementById("password").value;
+
+
+signInWithEmailAndPassword(
+auth,
+email,
+password
+)
+
+.then(()=>{
+
+window.location.href="index.html";
+
+})
+
+.catch((error)=>{
+
+document.getElementById("msg").innerHTML =
+error.message;
+
+});
+
+}
